7dfa8eb90e9610431f0fa564927430a85465b46a
Victoria <tovictoriadao@gmail.com>
2024-10-01 14:42:59 -0400

    Worked on Todo 1 and 2 Used the callback to get the textSize. Then in the callback, created an intent to pass the textSize to the second activity (DisplayActivity)

--- app/src/main/java/edu/temple/activities/MainActivity.kt
+++ app/src/main/java/edu/temple/activities/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,6 +9,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
+const val DISPLAY_KEY = "DisplayActivityKey"
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,16 +21,21 @@ class MainActivity : AppCompatActivity() {
         val textSizes = Array(20){(it + 1) * 5}
 
         Log.d("Array values", textSizes.contentToString())
+        //When the callback is called, get the position from the array and place it in an intent extra
+        //Then sends the intent to DisplayActivity
+        val callback = {item : Int ->
+            val launchIntent = Intent(this@MainActivity, DisplayActivity::class.java)
+            launchIntent.putExtra(DISPLAY_KEY, item)
+            startActivity(launchIntent)
+        }
+
 
         with (findViewById<RecyclerView>(R.id.textSizeSelectorRecyclerView)) {
 
             // TODO Step 2: Implement lambda body to launch new activity and pass value
-            adapter = TextSizeAdapter(textSizes){
-
-            }
+            adapter = TextSizeAdapter(textSizes, callback)
             layoutManager = LinearLayoutManager(this@MainActivity)
         }
-
 
 
     }
@@ -36,12 +43,14 @@ class MainActivity : AppCompatActivity() {
 
 
 /* Convert to RecyclerView.Adapter */
-class TextSizeAdapter (private val textSizes: Array<Int>, callback: (Int)->Unit) : RecyclerView.Adapter<TextSizeAdapter.TextSizeViewHolder>() {
+class TextSizeAdapter (private val textSizes: Array<Int>, private val callback: (Int)->Unit) : RecyclerView.Adapter<TextSizeAdapter.TextSizeViewHolder>() {
 
     // TODO Step 1: Complete onClickListener to return selected number
     inner class TextSizeViewHolder(val textView: TextView) : RecyclerView.ViewHolder (textView) {
         init {
-            textView.setOnClickListener {  }
+            textView.setOnClickListener {
+                //When the textView is clicked, return the number in the array at the position
+                callback(textSizes[adapterPosition])}
         }
     }
 
